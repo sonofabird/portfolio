d5e84e2e3706950d6a8bdcaa8dbfdc80f8f8d8e7
Mackenzie Dalton <kenzied@shaw.ca>
2024-04-25 22:31:35 -0400

    add router

---
+++ src/pages/Project1.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+const Project1 = () => {
+    return(
+        <div>
+             <Button href="/">Go Home</Button>
+        </div>
+    )
+}
+
+export default Project1;

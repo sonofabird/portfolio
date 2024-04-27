3890278fae966ded8f283064d9440915460d12c9
Mackenzie Dalton <kenzied@shaw.ca>
2024-04-27 17:54:48 -0400

    home page text+image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,21 @@
-import { Button } from "@mui/material";
-import { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import header_illust from '../assets/header_illust.png'
 
 const Home = () => {
     
-    const [count, setCount] = useState(0);
-    const handleClick = () => {
-        setCount(count + 1)
-    }
     
     return(
-        <div>
-            <Button onClick={()=>handleClick()}>{count}</Button>
-            <Button href="/Project1">Go to Project 1</Button>
-        </div>
+        <Box display="flex" alignContent="center" sx={{width:"100%"}}> 
+            <Box textAlign="left" sx={{width:"300px"}} display="flex" flexDirection="column">
+                <Typography variant="h1">
+                    kenzie hisako dalton 
+                </Typography> 
+                <Typography variant="body1">
+                    designer/illustrator/programmer
+                </Typography>
+            </Box>
+            <img src={header_illust} alt="its a me" height="600px"/>
+        </Box>
     )
 }
 

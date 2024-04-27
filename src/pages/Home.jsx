c6d3ed548fad240bfcf194d1d5d7ee852eab951c
joshchyeonlee <63981671+joshchyeonlee@users.noreply.github.com>
2024-04-27 16:25:42 -0600

    home page styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,17 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import header_illust from '../assets/header_illust.png'
 
 const Home = () => {
-    
-    
+
     return(
-        <Box display="flex" alignContent="center" sx={{width:"100%"}}> 
-            <Box textAlign="left" sx={{width:"300px"}} display="flex" flexDirection="column">
-                <Typography variant="h1">
-                    kenzie hisako dalton 
-                </Typography> 
-                <Typography variant="body1">
-                    designer/illustrator/programmer
-                </Typography>
+        <Box sx={{ width: "100%" }} display="flex" justifyContent="space-between">
+            <Box sx={{ width: "50%" }} display="flex" flexDirection="column" justifyContent="center" textAlign="left" paddingLeft="100px">
+                <Typography variant="h1">kenzie hisako dalton</Typography>
+                <Typography variant="body1">designer/illustrator/programmer</Typography>
             </Box>
-            <img src={header_illust} alt="its a me" height="600px"/>
+            <Box display="flex" sx={{position: "absolute", bottom: 0, right: "100px" }}>
+                <img src={header_illust} alt="its a me" height="600px"/>
+            </Box>
         </Box>
     )
 }

bdf8d53277a484310fe977b10c0c39069aa1fae2
Mackenzie Dalton <kenzied@shaw.ca>
2024-05-14 14:38:33 -0600

    added tape assets

--- src/components/NiceToMeetYou.jsx
+++ src/components/NiceToMeetYou.jsx
@@ -1,25 +1,36 @@
 import {Box, Card, Typography } from "@mui/material";
+import grid_tape from '../assets/grid_tape.png'
 
 
+const Tape = (props) => {
+
+    return(
+        <Box>
+            <img height={75} src={props.tape}/>
+        </Box>
+    )
+}
 
 const NiceToMeetYou = () => {
 
     return(
-       
-        <Card 
-            position="absolute"
-            
-            sx={{
-                bgcolor: '#000000',
-                marginTop: -4
-            }}        
-        >
-            <Box paddingY={8} paddingX={12}>
-                <Typography paddingBottom={4} variant='h3' align ='left' color='#ffffff'>nice to meet you!</Typography>
-                <Typography align='left' color='#ffffff'>I am a product designer with a background in illustration and computer science! I love combining creativity and technicality to make robust designs.</Typography>
-            </Box>
-        </Card>
-      
+        <>
+
+            <Tape isTop={true} tape={grid_tape}/>
+            <Card 
+                position="absolute"
+                
+                sx={{
+                    bgcolor: '#000000'
+                }}        
+            >
+                <Box paddingY={8} paddingX={12}>
+                    <Typography paddingBottom={4} variant='h3' align ='left' color='#ffffff'>nice to meet you!</Typography>
+                    <Typography align='left' color='#ffffff'>I am a product designer with a background in illustration and computer science! I love combining creativity and technicality to make robust designs.</Typography>
+                </Box>
+            </Card>
+            <Tape isTop={false} tape={grid_tape}/>
+        </>
     )
 
 }

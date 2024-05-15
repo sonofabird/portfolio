f0c4fc4a5642d77913edc5ba1777570bbf4ed093
joshchyeonlee <joshchyeonlee@gmail.com>
2024-05-14 20:33:09 -0600

    update styling for tape

--- src/components/NiceToMeetYou.jsx
+++ src/components/NiceToMeetYou.jsx
@@ -5,25 +5,33 @@ import grid_tape2 from '../assets/grid_tape2.png'
 const Tape = (props) => {
 
     return(
-        <Box position="relative" marginTop={ props.isTop ? 10 : -5} zIndex={10}>
-            <img height={75} src={props.tape} />
-        </Box>
+        <Box
+            position="relative"
+            component="img"
+            top={ props.isTop ? 40 : -40}
+            sx={{
+                height: 75,
+                zIndex: 'tooltip',
+                opacity: 0.85,
+            }}
+            src={props.tape}
+        />
     )
 }
 
 const NiceToMeetYou = () => {
 
     return(
-        <>
+        <Box position="relative" sx={{ width: "100%" }} justifyContent="center" top={-100}>
             <Tape isTop={true} tape={grid_tape2}/>
-            <Card position="absolute" sx={{ bgcolor: '#000000'}}>
-                <Box paddingY={8} paddingX={12}>
+            <Card sx={{ bgcolor: '#000000', borderRadius: 8 }} >
+                <Box paddingY={16} paddingX={12}>
                     <Typography paddingBottom={4} variant='h3' align ='left' color='#ffffff'>nice to meet you!</Typography>
                     <Typography align='left' color='#ffffff'>I am a product designer with a background in illustration and computer science! I love combining creativity and technicality to make robust designs.</Typography>
                 </Box>
             </Card>
             <Tape isTop={false} tape={grid_tape2}/>
-        </>
+        </Box>
     )
 
 }

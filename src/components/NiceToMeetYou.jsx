e94db7e8708cd66e2e04fcf0c86dc7d30ca3a017
Mackenzie Dalton <kenzied@shaw.ca>
2024-05-14 20:15:01 -0600

    adding assets

--- src/components/NiceToMeetYou.jsx
+++ src/components/NiceToMeetYou.jsx
@@ -1,12 +1,12 @@
 import {Box, Card, Typography } from "@mui/material";
-import grid_tape from '../assets/grid_tape.png'
+import grid_tape2 from '../assets/grid_tape2.png'
 
 
 const Tape = (props) => {
 
     return(
-        <Box>
-            <img height={75} src={props.tape}/>
+        <Box position="relative" marginTop={ props.isTop ? 10 : -5} zIndex={10}>
+            <img height={75} src={props.tape} />
         </Box>
     )
 }
@@ -15,21 +15,14 @@ const NiceToMeetYou = () => {
 
     return(
         <>
-
-            <Tape isTop={true} tape={grid_tape}/>
-            <Card 
-                position="absolute"
-                
-                sx={{
-                    bgcolor: '#000000'
-                }}        
-            >
+            <Tape isTop={true} tape={grid_tape2}/>
+            <Card position="absolute" sx={{ bgcolor: '#000000'}}>
                 <Box paddingY={8} paddingX={12}>
                     <Typography paddingBottom={4} variant='h3' align ='left' color='#ffffff'>nice to meet you!</Typography>
                     <Typography align='left' color='#ffffff'>I am a product designer with a background in illustration and computer science! I love combining creativity and technicality to make robust designs.</Typography>
                 </Box>
             </Card>
-            <Tape isTop={false} tape={grid_tape}/>
+            <Tape isTop={false} tape={grid_tape2}/>
         </>
     )
 

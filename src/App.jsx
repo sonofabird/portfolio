19bc066fe46e742cc7632f069cf11c450e7b27b0
joshchyeonlee <joshchyeonlee@gmail.com>
2024-06-15 14:37:28 -0600

    add tile list

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header'
 import './App.css'
 import TileGrid from './components/TileGrid'
 import Proj1Desc from './assets/p1/Proj1Desc'
+import TileList from './components/TileList'
+import Proj1Features from './assets/p1/Proj1Features'
 
 function App() {
 
@@ -11,6 +13,7 @@ function App() {
      <Header/>
      <Bio/>
      <TileGrid description={Proj1Desc}/>
+     <TileList description={Proj1Features.description} features={Proj1Features.features}/>
     </>
   )
 }

7ab3ad6ce87b0b2c326d85c755bd046112422dc9
Mackenzie Dalton <kenzied@shaw.ca>
2024-06-27 14:59:24 -0600

    fixed header spacing

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,20 +1,21 @@
 import { Box, Typography } from "@mui/material";
 import grid_tape from "../assets/home/grid_tape 1.png";
-import pink_tape from "../assets/home/pink_tape.png"
+import pink_tape from "../assets/home/pink_tape.png";
 
 const Bio = () => {
   return (
-    <Box paddingBottom="30%">
+    <Box paddingBottom="30%" paddingX="5%">
       <Box position="absolute" marginTop={-8} paddingLeft="10%">
-        <img src={grid_tape}></img>
+        <img src={grid_tape} width="100%"></img>
       </Box>
 
-      <Box position="absolute" marginTop={-10} paddingLeft="3%" sx={{opacity:"70%"}}>
+      <Box
+        position="absolute"
+        marginTop={-10}
+        paddingLeft="3%"
+        sx={{ opacity: "70%" }}
+      >
         <img src={pink_tape} width="50%"></img>
-      </Box>
-
-      <Box position="absolute" marginTop={74} paddingLeft="50%">
-        <img src={grid_tape}></img>
       </Box>
 
       <Box
@@ -23,8 +24,7 @@ const Bio = () => {
         paddingX="142px"
         paddingY="212px"
         borderRadius="35px"
-        sx={{boxShadow:4}}
-        
+        sx={{ boxShadow: 4 }}
       >
         <Typography variant="h3" paddingBottom="5%" color="#FFFFFF">
           nice to meet you!
@@ -44,6 +44,9 @@ const Bio = () => {
           </b>
           ! I love combining creativity and technicality to make robust designs.
         </Typography>
+      </Box>
+      <Box position="absolute" marginTop={-10} paddingLeft="50%">
+        <img src={grid_tape} width="100%"></img>
       </Box>
     </Box>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,13 @@ const Header = () => {
   return (
     <Element name="/">
       <Box display="flex" paddingX="10%" paddingTop="5%">
-        <Box textAlign="left" paddingTop="10%" width="100%">
-          <Typography lineHeight={1} variant="title">
-            kenzie hisako dalton
-          </Typography>
-          <Typography fontSize={24}>
-            product designer + illustrator + programmer
-          </Typography>
+        <Box textAlign="left" paddingTop="12%" width="100%">
+          <img src={Name}></img>
+          <Box paddingY="5%" paddingX="5%">
+            <Typography fontSize={24}>
+              product designer + illustrator + programmer
+            </Typography>
+          </Box>
         </Box>
 
         <img src={Icon} width="45%"></img>

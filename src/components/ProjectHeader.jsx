4f49595783c9e6c0810a87d7c61108704e1866c7
Mackenzie Dalton <kenzied@shaw.ca>
2024-06-26 21:15:37 -0600

    oops

--- src/components/ProjectHeader.jsx
+++ src/components/ProjectHeader.jsx
@@ -1,24 +1,33 @@
 import { Box, Typography } from "@mui/material";
 import project_head from "../assets/p1/project_head.png";
 import preview from "../assets/p1/preview.png";
+import { Element } from "react-scroll";
 
 const ProjectHeader = () => {
+  console.log(window.innerWidth);
+
   return (
-    <Box sx={{ height: "110vh" }} paddingX="10%" paddingBottom="10%">
-      <Box>
-        <img src={project_head} />
-        <Typography variant="bullets" textAlign={"left"} width={500}>
-          Mentra AI is a platform in which starting entrepreneurs can receive
-          guidance while building a business plan in a low-pressure environment.
-          The platform is centred around the Mentra AI chatbot, which gives
-          users advice, suggestions, resources and connections relevant to their
-          business plan.
-        </Typography>
+    <Element name="/overview">
+      <Box paddingY="10%">
+        <Box width="100%" paddingX="5%" display="flex" justifyContent="center">
+          <img src={project_head} width="100%" />
+        </Box>
+        <Box display="flex" flexDirection="horizontal" width="100%">
+          <Box paddingY="5%" paddingX="5%">
+            <Typography variant="bullets" textAlign={"left"} width={500}>
+              Mentra AI is a platform in which starting entrepreneurs can
+              receive guidance while building a business plan in a low-pressure
+              environment. The platform is centred around the Mentra AI chatbot,
+              which gives users advice, suggestions, resources and connections
+              relevant to their business plan.
+            </Typography>
+          </Box>
+          <Box marginTop={-10} width="100%">
+            <img src={preview} />
+          </Box>
+        </Box>
       </Box>
-      <Box position="absolute" marginTop={-50} right={0}>
-        <img src={preview} />
-      </Box>
-    </Box>
+    </Element>
   );
 };
 

06ad652dadd816a0c2ad7b50af02e0ff40ff9990
Joshua Lee <63981671+joshchyeonlee@users.noreply.github.com>
2024-06-27 14:47:00 -0600

    Responsive formatting (#10)
    * update bio
    
    * update projectheader
    
    * update tape behavior

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 //import "./index.css";
-import { ThemeProvider } from "@emotion/react";
-import theme from "./themes/themes"
+import { ThemeProvider } from '@emotion/react';
+import theme from './themes/themes';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
-      <App />
-    
+    <App />
   </React.StrictMode>
 );
--- src/themes/themes.jsx
+++ src/themes/themes.jsx
@@ -1,68 +1,68 @@
-import { createTheme } from "@mui/material";
-import Inconsolata from "../fonts/Inconsolata/static/Inconsolata_UltraExpanded-Black.ttf";
-import Epilogue from "../fonts/Epilogue/Epilogue-Italic-VariableFont_wght.ttf";
+import { createTheme } from '@mui/material';
+import Inconsolata from '../fonts/Inconsolata/static/Inconsolata_UltraExpanded-Medium.ttf';
+import Epilogue from '../fonts/Epilogue/Epilogue-Italic-VariableFont_wght.ttf';
 
 const theme = createTheme({
   palette: {
     background: {
-      default: "#FFFAF8",
+      default: '#FFFAF8',
     },
     primary: {
-      main: "#4F772D",
+      main: '#4F772D',
     },
     secondary: {
-      main: "#6A994E",
+      main: '#6A994E',
     },
     error: {
-      main: "#BC4749",
+      main: '#BC4749',
     },
   },
   typography: {
-    fontFamily: ["Epilogue", "sans-serif"].join(","),
+    fontFamily: [{ Epilogue }, 'sans-serif'].join(','),
     fontSize: 16,
-    title:{
-      fontFamily:"Inconsolata UltraExpanded",
-      fontSize:"96px",
-      fontWeight:900
+    title: {
+      fontFamily: { Inconsolata },
+      fontSize: '96px',
+      fontWeight: 900,
     },
     h1: {
-      color: "#3A00B5",
-      fontSize: "64px",
+      color: '#3A00B5',
+      fontSize: '64px',
       fontWeight: 700,
     },
     h2: {
-      color: "#3A00B5",
-      fontSize: "40px",
+      color: '#3A00B5',
+      fontSize: '40px',
       fontWeight: 500,
     },
     h3: {
-      fontFamily: "Cormorant Garamond",
-      fontSize: "64px",
-      fontStyle: "normal",
-      fontWeight: 500
+      fontFamily: 'Cormorant Garamond',
+      fontSize: '64px',
+      fontStyle: 'normal',
+      fontWeight: 500,
     },
     h4: {
-      color: "#8A82BD",
+      color: '#8A82BD',
       fontSize: 24,
       fontWeight: 500,
     },
     b1: {
-      color: "#3A00B5",
+      color: '#3A00B5',
       fontSize: 24,
     },
-    caption:{
-      color:"#8A82BD",
-      fontSize: "16px",
+    caption: {
+      color: '#8A82BD',
+      fontSize: '16px',
     },
-    navbar:{
-      color:"#000000",
-      fontFamily:["Proza Libre", "sans-serif"].join(","),
-      fontSize: "16px",
-      fontWeight: 900
+    navbar: {
+      color: '#000000',
+      fontFamily: ['Proza Libre', 'sans-serif'].join(','),
+      fontSize: '16px',
+      fontWeight: 900,
     },
-    bullets:{
-      color: "#3A00B5",
-      fontSize: "28px",
+    bullets: {
+      color: '#3A00B5',
+      fontSize: '28px',
       fontWeight: 500,
     },
   },
@@ -70,14 +70,14 @@ const theme = createTheme({
     MuiCard: {
       styleOverrides: {
         root: {
-          background: "#FFF9EC",
+          background: '#FFF9EC',
         },
       },
     },
     MuiBottomNavigation: {
       styleOverrides: {
         root: {
-          background: "#FFF9EC",
+          background: '#FFF9EC',
         },
       },
     },

67ab3b21ce14c14a1165b7b5202725dc7ec0fc20
Mackenzie Dalton <kenzied@shaw.ca>
2024-06-27 17:21:44 -0600

    image caption responsive

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,11 +3,22 @@ import { maxWidth } from "@mui/system";
 
 const Image = (props) => {
   return (
-    <Box textAlign="left" display="absolute" paddingX="10%" paddingY="5%">
-      <img src={props.imageSRC} width="100%" />
-      <Box paddingX="5%">
-        <Typography variant="caption">{props.caption}</Typography>
-      </Box>
+    <Box>
+      {screen.width > 400 ? (
+        <Box textAlign="left" display="absolute" paddingX="10%" paddingY="5%">
+          <img src={props.imageSRC} width="100%" />
+          <Box paddingX="5%">
+            <Typography variant="caption">{props.caption}</Typography>
+          </Box>
+        </Box>
+      ) : (
+        <Box textAlign="left" display="absolute" paddingX="10%" paddingY="5%">
+          <img src={props.imageSRC} width="100%" />
+          <Box paddingX="5%">
+            <Typography fontSize="50%" variant="caption">{props.caption}</Typography>
+          </Box>
+        </Box>
+      )}
     </Box>
   );
 };

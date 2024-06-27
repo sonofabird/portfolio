9b5a001dd83e021ed7e6ca3ea20102e8d1cb035a
Mackenzie Dalton <kenzied@shaw.ca>
2024-06-27 16:12:06 -0600

    responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,18 +6,33 @@ import Name from "../assets/home/NAME.png";
 const Header = () => {
   return (
     <Element name="/">
-      <Box display="flex" paddingX="10%" paddingTop="5%">
-        <Box textAlign="left" paddingTop="10%" width="100%">
-          <img src={Name} />
-          <Box paddingX="4%" paddingY="4%">
-            <Typography fontSize={24}>
-              product designer + illustrator + programmer
-            </Typography>
+      {screen.width > 400 ? (
+        <Box display="flex" paddingX="10%" paddingTop="5%">
+          <Box textAlign="left" paddingTop="10%" width="100%">
+            <img src={Name} />
+            <Box paddingX="4%" paddingY="4%">
+              <Typography fontSize={24}>
+                product designer + illustrator + programmer
+              </Typography>
+            </Box>
           </Box>
-        </Box>
 
-        <img src={Icon} width="45%"></img>
-      </Box>
+          <img src={Icon} width="45%"></img>
+        </Box>
+      ) : (
+        <Box display="flex" paddingX="10%" paddingTop="5%">
+          <Box textAlign="left" paddingTop="10%" width="100%">
+            <img src={Name} width="85%" />
+            <Box paddingX="5%" paddingY="4%">
+              <Typography fontSize={6}>
+                product designer + illustrator + programmer
+              </Typography>
+            </Box>
+          </Box>
+
+          <img src={Icon} width="45%"></img>
+        </Box>
+      )}
     </Element>
   );
 };
